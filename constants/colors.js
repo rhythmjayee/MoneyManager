80365d829ca1e4b8a9e125f1dea9f77e27d60016
rhythmjayee <jayeerythm8@gmail.com>
2022-12-25 14:11:28 +0530

    added colors

---
+++ constants/colors.js
@@ -0,0 +1,9 @@
+const GlobalColors = {
+    dark: '#080705ff',
+    charcoal: '#40434eff',
+    wine: '#702632ff',
+    wineLight: '#912f40ff',
+    light: '#fffffaff',
+}
+
+export default GlobalColors;
