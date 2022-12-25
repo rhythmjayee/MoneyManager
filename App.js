1baf25946eb6d6523082234bbe2e070fbb997756
rhythmjayee <jayeerythm8@gmail.com>
2022-12-25 14:29:49 +0530

    added packages

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
+import 'react-native-gesture-handler'; //Need to be on top as mentioned in docs -> https://reactnavigation.org/docs/stack-navigator
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
+    return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
     </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # MoneyManager
+NOTE : Project is created using `npx create-expo-app <APP NAME>`
+
+## Install Dependencies
+`npm install`
+
+## Packages Used
+1. React Navigation  
+    1.1 `npm install @react-navigation/native`  
+    1.2 `npx expo install react-native-screens react-native-safe-area-context`  
+    1.3 `npm install @react-navigation/stack`  
+    1.4 `npx expo install react-native-gesture-handler`  
+    1.5 `npm install @react-navigation/native-stack`  
+    1.6 `npm install @react-navigation/bottom-tabs`  
+2. Expo Splash Screen  
+`npx expo install expo-splash-screen`
