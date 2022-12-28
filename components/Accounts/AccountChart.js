09e077224c01f52ea8dc708e6b3d5030f3768bff
rhythmjayee <jayeerythm8@gmail.com>
2022-12-28 14:55:32 +0530

    added pie chart component

---
+++ components/Accounts/AccountChart.js
@@ -0,0 +1,65 @@
+import { View, StyleSheet, Text } from "react-native"
+import { PieChart } from "react-native-chart-kit"
+import { Dimensions } from "react-native";
+import IconButton from "../UI/IconButton"
+import GlobalColors from "../../constants/colors"
+import { convertNumberToCurrency } from "../../utils/helper";
+
+const screenWidth = Dimensions.get("window").width;
+const chartConfig = {
+    backgroundGradientFrom: GlobalColors.wine100,
+    backgroundGradientFromOpacity: 1,
+    backgroundGradientTo: GlobalColors.wine1200,
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 3, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+};
+
+const AccountChart = ({Title, data, amount}) => {
+    return (
+    <View style={styles.chart}>
+        <View style={styles.accountHead}>
+            <IconButton size={20} color={GlobalColors.light500} icon={'wallet-outline'}/>
+            <Text style={styles.Title}>{Title}: {convertNumberToCurrency('INR', amount)}</Text>
+        </View>
+        <PieChart
+        data={data}
+        width={screenWidth}
+        height={250}
+        chartConfig={chartConfig}
+        accessor={"amount"}
+        backgroundColor={'#ffffff00'}
+        paddingLeft={"15"}
+        center={[0, 0]}
+        absolute
+        />
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    chart: {
+        
+    },
+    Title: {
+        color: GlobalColors.light500,
+        fontFamily: 'Walkway-bk',
+        fontSize: 20,
+        marginLeft: 5,
+        marginRight: 5,
+    },
+    accountHead: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        backgroundColor: GlobalColors.wine900,
+        alignItems: 'center',
+        padding: 10,
+        margin: 10,
+        borderRadius: 30,
+        
+    },
+})
+
+export default AccountChart
--- screens/AccountsChartScreen.js
+++ screens/AccountsChartScreen.js
@@ -1,59 +1,60 @@
 import { useState, useEffect, useContext } from "react";
-import { View, StyleSheet } from "react-native"
-import { PieChart } from "react-native-chart-kit";
-import { Dimensions } from "react-native";
+import { ScrollView, StyleSheet, Text, Dimensions, View } from "react-native"
 import GlobalColors from "../constants/colors";
 import { shades } from "../constants/colors";
 import { AccountContext } from "../store/accounts-context";
-import { convertNumberToCurrency } from "../utils/helper";
+import AccountChart from "../components/Accounts/AccountChart";
 
-const screenWidth = Dimensions.get("window").width;
-const chartConfig = {
-    backgroundGradientFrom: "#1E2923",
-    backgroundGradientFromOpacity: 0,
-    backgroundGradientTo: "#08130D",
-    backgroundGradientToOpacity: 0.5,
-    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-    strokeWidth: 2, // optional, default 3
-    barPercentage: 0.5,
-    useShadowColorFromDataset: false // optional
-};
-
-
+const screenHeight = Dimensions.get("window").height;
 const AccountsChartScreen = () => {
-    const {accounts : {all}} = useContext(AccountContext)
-    const [data, setData] = useState([]);
+    const {accounts : {all, amount}} = useContext(AccountContext)
+    const [data, setData] = useState({
+        "Accounts" : [[],0]
+    });
     
     useEffect(() => {
-        let data = Object.entries(all).map(([account, val], index) => {
+        let newData = {}
+        newData["Accounts"] = []
+        let arr = Object.entries(all).map(([account, val], index) => {
             let i = ((shades.length - index) + shades.length - 1) % shades.length
-            console.log(i)
             let color = shades[i]
             return {
                 name: account,
                 amount: val.amount,
                 color: color,
                 legendFontColor: GlobalColors.light500,
-                legendFontSize: 15
+                legendFontSize: 12
             }
         })
-        setData(data)
+        newData["Accounts"].push(arr)
+        newData["Accounts"].push(amount)
+
+        Object.entries(all).map(([account, val]) => {
+            newData[account] = []
+            arr = Object.entries(val.subAccounts).map(([subAccount, subVal], subIndex) => {
+                let i = ((shades.length - subIndex) + shades.length - 1) % shades.length
+                let color = shades[i]
+                return {
+                    name: subAccount,
+                    amount: subVal.amount,
+                    color: color,
+                    legendFontColor: GlobalColors.light500,
+                    legendFontSize: 12
+                }
+            })
+            newData[account].push(arr)
+            newData[account].push(val.amount)
+        })
+        setData(newData)
     }, [all]);
+
     return (
         <View style={styles.container}>
-            <View style={styles.chart}>
-                <PieChart
-                data={data}
-                width={screenWidth}
-                height={250}
-                chartConfig={chartConfig}
-                accessor={"amount"}
-                backgroundColor={'#ffffff00'}
-                paddingLeft={"15"}
-                center={[0, 0]}
-                absolute
-                />
-            </View>
+            <ScrollView contentContainerStyle={styles.scroll}>
+            {Object.entries(data).map(([key, val], index) => {
+                return <AccountChart key={index} Title={key} data={val[0]} amount={val[1]}/>
+            })}
+            </ScrollView>
         </View>
     )
 }
@@ -61,12 +62,11 @@ const AccountsChartScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1, 
-        padding: 10,
-        alignItems: 'center'
+        padding: 5,
+        height: '80%'
     },
-    chart: {
-        margin: 10,
-        borderRadius: 20
+    scroll: {
+        alignItems: 'center',
     }
 })
 
